1187f224f0a01a5b52b2ba67fb44b1f957103906
Pid <pid@pidster.com>
2012-04-14 19:05:05 +0100

    fiddling

--- public/js/ponk.js
+++ public/js/ponk.js
@@ -4,7 +4,9 @@ sock.onopen = function() {
 };
 sock.onmessage = function(e) {
 	console.log('message', e.data);
+	log("Sending username...");
 };
+
 sock.onclose = function() {
 	console.log('close');
 };
@@ -14,13 +16,20 @@ $('#signin-button').click( function() {
 });
 
 $('#signin').submit( function() {
-	$('#login-window').hide();
-
 	var username = $("input#username").val();
-	sock.send(username);
+	if (username == null || username == '') {
+		$("input#username").
+		return false;
+	}
 
+	$('#login-window').hide();
 	$('#log-window').show();
 
+	log("Sending username...");
+	sock.send(username);
 	return false;
 });
 
+function log(msg) {
+	$('#log-window').append('<p>' + msg '</p>');
+}
