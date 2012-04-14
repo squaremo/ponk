fdd11f5acf9801e1f08a14d789e7f05bdde7fd4c
Pid <pid@pidster.com>
2012-04-14 18:21:28 +0100

    make jquery work by actually adding it

--- public/js/ponk.js
+++ public/js/ponk.js
@@ -1,2 +1,26 @@
-var sock = new SockJS('');
+var sock = new SockJS('/socks');
+sock.onopen = function() {
+	console.log('open');
+};
+sock.onmessage = function(e) {
+	console.log('message', e.data);
+};
+sock.onclose = function() {
+	console.log('close');
+};
+
+$('#signin-button').click( function() {
+	$('#signin').submit();
+});
+
+$('#signin').submit( function() {
+	$('#login-window').hide();
+
+	var username = $("input#username").val();
+	sock.send(username);
+
+	$('#log-window').show();
+
+	return false;
+});
 
